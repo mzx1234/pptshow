36099930e7054f9d1396868c48a05702e09c403c
mzx1234 <316701116@qq.com>
2016-01-09 13:45:20 +0800

    test rm

--- ppt-procotol/target/classes/service/TOptionService.thrift
+++
@@ -1,6 +0,0 @@
-include "../struct/PPTContentStruct.thrift"
-namespace java com.mzx.pptprocotol.thrift.service
-
-service TOptionService {
-    PPTContentStruct.PPTBytes swichPPTPage(1:PPTContentStruct.PPTDetail parm),
-}
--- ppt-procotol/target/classes/service/TParsePPTService.thrift
+++
@@ -1,6 +0,0 @@
-include "../struct/PPTContentStruct.thrift"
-namespace java com.mzx.pptprocotol.thrift.service
-
-service TParsePPTService {
-    PPTContentStruct.PPTBytes parsePPTAndGetFirst(1:PPTContentStruct.PPTDetail parm),
-}
